e7c76fb79713212578bb115b571a836cbfdfa091
unknown <ahallmann@.int.oraise.de>
2010-04-12 15:54:08 +0200

    implemented suggestions from http://sacharya.com/grails-dbcp-stale-connections/

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,4 +1,13 @@
 // Place your Spring DSL code here
 beans = {
-    
+    dataSource(BasicDataSource) {
+        minEvictableIdleTimeMillis=1800000
+        timeBetweenEvictionRunsMillis=1800000
+        numTestsPerEvictionRun=3
+
+        testOnBorrow=true
+        testWhileIdle=true
+        testOnReturn=true
+        validationQuery="SELECT 1"
+    }
 }
